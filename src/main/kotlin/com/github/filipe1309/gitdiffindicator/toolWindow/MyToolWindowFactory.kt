29fc0361be3fcb959c915fae4735fa29cefa881c
GitHub Action <action@github.com>
2024-11-08 13:23:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/filipe1309/gitdiffindicator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/filipe1309/gitdiffindicator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.filipe1309.gitdiffindicator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.filipe1309.gitdiffindicator.MyBundle
+import com.github.filipe1309.gitdiffindicator.services.MyProjectService
 import javax.swing.JButton
 
 
